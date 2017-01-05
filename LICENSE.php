74876d4b72e93e5ceb2837835d8981e59656e01c
alexz <alexnew2000@mail.ru>
2017-01-06 00:23:04 +0300

    eto na timeveb

--- LICENSE.php
+++ LICENSE.php
@@ -1,21 +1,3 @@
 <?php
 
-use yii\helpers\Html;
-use yii\helpers\Html;
-1111
-1111
-2222
-
-2222
-2222
-2222
-22223333
-2222
-22312312212312
-12312312
-sdfsdfsdfsdfsdfsdfsadf
-asdf
-asdfsad
-fas
-df
-asdf
+echo 'Это отправляем на сервак time veb';
